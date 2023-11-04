7bf5d7ab650868d9fc421b1f2d89c42fce4474b9
janeblancaocampo <qjbaocampo@tip.edu.ph>
2023-11-04 19:12:08 +0800

    updated login and signup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './App.css';
+import LoginSignUp from "./LoginSignUp/LoginSignUp";
+
+function App() {
+  return (
+    <LoginSignUp />
+  );
+}
+
+export default App;
