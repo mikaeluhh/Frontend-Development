aa7459d9137102a718e481ff62507c7d3b2729f0
chanmusni <133565464+chanmusni@users.noreply.github.com>
2023-11-07 20:43:14 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import Navbar from "./components/Navbar/Navbar";
 import Intro from "./components/Introduction/Intro";
+import History from "./components/History/History"; 
 // import Tutorial from './components/Tutorial/Tutorial';
 // import LoginSignUp from './components/LoginSignUp/LoginSignUp';
-// import Upload from "./components/Upload/Upload";
+import Upload from "./components/Upload/Upload";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/> 
-      <Intro/> 
-      {/* <Upload/>   */}
+      <Navbar/>
+      <Intro/>  
+      {/* <Intro/>  */}
       {/* <Tutorial/> */}
+      <Upload/>  
       {/* <LoginSignUp/> */}
+      <History/>
     </div>
   );
 }
